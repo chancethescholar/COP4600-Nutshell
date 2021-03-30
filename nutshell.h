b4d0a49587e0f9d6623bfdbb29c520ba36adbe33
Chance C. Onyiorah <51178636+chancethescholar@users.noreply.github.com>
2021-03-30 12:43:07 -0400

    Create nutshell.h

---
+++ nutshell.h
@@ -0,0 +1,14 @@
+ #include <stdio.h>
+ #include <string.h>
+
+int builtin;  //Determines if command is built in or not
+int command = -1;
+
+void yerror();
+int yylex();
+int yyparse();
+int chdir();
+
+int getCommand();
+void execute();
+
