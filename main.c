8090037307d9ded412076e6f14b85dc8ac072a77
Chance <chanceony@gmail.com>
2021-04-15 00:53:27 -0400

    env

--- main.c
+++ main.c
@@ -173,8 +173,9 @@ void execute()
 
 		else if(strcmp(commandTable[i].comName, "setenv") == 0)
 		{
-			if(commandTable[i].args[1] != NULL && commandTable[i].args[2] != NULL && commandTable[i].args[3] == NULL){
-				runSetEnv(commandTable[i].args[1],commandTable[i].args[2]);
+			if(commandTable[i].args[1] != NULL && commandTable[i].args[2] != NULL && commandTable[i].args[3] == NULL)
+      {
+				runSetEnv(commandTable[i].args[1], commandTable[i].args[2]);
 			}
 
 			else
